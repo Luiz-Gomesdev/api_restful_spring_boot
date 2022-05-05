ca6f75206822c1c5215691d033e0339141006fc4
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-05-05 10:54:33 -0300

    Entity User and REST working

--- src/main/java/com/luizgomes/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/luizgomes/workshopmongo/WorkshopmongoApplication.java
@@ -1,4 +1,4 @@
-package com.nelioalves.workshopmongo;
+package com.luizgomes.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
