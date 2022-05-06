ff09f9e145b43221fec1cf4c8e0bdfc545a215f7
Luiz Gomes <lgomes_kiau@hotmail.com>
2022-05-06 09:53:24 -0300

    User insertion with POST

--- src/main/java/com/luizgomes/workshopmongo/services/UserService.java
+++ src/main/java/com/luizgomes/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luizgomes.workshopmongo.domain.User;
+import com.luizgomes.workshopmongo.dto.UserDTO;
 import com.luizgomes.workshopmongo.repository.UserRepository;
 import com.luizgomes.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,13 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not Found"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
